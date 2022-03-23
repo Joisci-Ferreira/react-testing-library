3a2ba09c6caf62f27d23f61a0d8a3491a6027dd0
Joiscilene <joisci.lene@hotmail.com>
2022-03-23 01:23:57 -0300

    Requisitos 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import rederWithRouter from './RenderWithRouter';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Testando o componente FavoritePokemons.js', () => {
+  test('Verifica se é exibido na tela a mensagem "No favorite pokemon found",'
+   + 'se a pessoa não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+
+    const notFavorite = screen.getByText('No favorite pokemon found');
+    expect(notFavorite).toBeInTheDocument();
+  });
+
+  test('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    rederWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favoriteCard = screen.getAllByRole('link', { name: /more details/i });
+    expect(favoriteCard.length).toBe(pokemons.length);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componebte NotFound.js', () => {
+  test('Verifica se página contém um heading h2 com o texto "Page requested not found 😭"',
+    () => {
+      render(<NotFound />);
+
+      const notFoundHeading = screen.getByRole('heading', {
+        level: 2, name: /Page requested not found/i,
+      });
+      expect(notFoundHeading).toBeInTheDocument();
+    });
+
+  test('Verifica se a página mostra a imagem', () => {
+    render(<NotFound />);
+
+    const notFoundImage = screen.getByAltText(/Page requested was not found/i);
+
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
